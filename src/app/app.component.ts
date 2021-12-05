3943dbf4fc162be6cda66bdf90378b67a8f7b5b0
Terin Sebastin <terins@kruxsoft.com>
2021-12-05 18:19:23 +0530

    Reactive Form Submitting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularFromsReactive';
+  genders = ['male', 'female'];
+  signupFor: FormGroup;
+
+  ngOnInit() {
+    this.signupFor = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      gender: new FormControl(null),
+    });
+  }
+  onSubmit() {
+    console.log(this.signupFor);
+  }
 }
